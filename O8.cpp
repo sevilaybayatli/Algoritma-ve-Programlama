f8cbb532e3f93d25453d91c6fa0f17c6d051b025
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-06 20:32:20 +0300

    Create O8.cpp

---
+++ O8.cpp
@@ -0,0 +1,17 @@
+/*Numeric Data Types
+Use int when you need to store a whole number without decimals, like 35 or 1000, and float or double when you need a floating point number (with decimals),
+like 9.99 or 3.14515.*/
+#include <iostream>
+using namespace std;
+ 
+int main () {
+  int myNum = 1000;
+  cout << myNum;
+  
+  float myNum = 5.75;
+  cout << myNum;
+  
+  double myNum = 19.99;
+  cout << myNum;
+  return 0;
+}
