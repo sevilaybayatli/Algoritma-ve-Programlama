6ce0c5dd8a3f3e00b4809c98e418c772104d6feb
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-06 20:38:53 +0300

    Create O10.cpp

---
+++ O10.cpp
@@ -0,0 +1,16 @@
+/*The char data type is used to store a single character. The character must be surrounded by single quotes, like 'A' or 'c':
+
+#include <iostream>
+using namespace std;
+ 
+int main () {
+
+char myGrade = 'B';
+cout << myGrade;
+//Alternatively, you can use ASCII values to display certain characters:
+  char a = 65, b = 66, c = 67;
+  cout << a;
+  cout << b;
+  cout << c;
+  return 0;
+}
