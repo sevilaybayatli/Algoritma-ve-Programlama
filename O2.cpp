a22feb77acd92c02a3a926109ae01e205bf49522
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-06 20:01:52 +0300

    Update O2.cpp

--- O2.cpp
+++ O2.cpp
@@ -18,3 +18,10 @@ Note: It is recommended to use descriptive names in order to create understandab
 	cout << m;
 	return 0;
 }
+/*The general rules for naming variables are:
+
+Names can contain letters, digits and underscores
+Names must begin with a letter or an underscore (_)
+Names are case sensitive (myVar and myvar are different variables)
+Names cannot contain whitespaces or special characters like !, #, %, etc.
+Reserved words (like C++ keywords, such as int) cannot be used as names*/
