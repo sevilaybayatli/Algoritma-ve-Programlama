d0d88530f81dddb7d50b1012f7c64e3ecdd40eb1
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-07 18:05:48 +0300

    Create O1.cpp

---
+++ Strings/O1.cpp
@@ -0,0 +1,14 @@
+/*Strings are used for storing text.
+
+A string variable contains a collection of characters surrounded by double quotes
+To use strings, you must include an additional header file in the source code, the <string> library:
+*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string greeting = "Hello";
+  cout << greeting;
+  return 0;
+}
