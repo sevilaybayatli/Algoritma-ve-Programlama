1e4ad83f0b5314ef177f5e7903291300a0e09414
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-07 17:56:49 +0300

    Create O21.cpp

---
+++ O21.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 5;
+  int y = 3;
+  cout << (x != y); // returns 1 (true) because 5 is not equal to 3
+  
+  int x1 = 5;
+  int y2 = 3;
+  cout << (x1 >= y1); // returns 1 (true) because five is greater than, or equal, to 3
+  return 0;
+}
