b79b6a9101078b741bc85f274efed8304855135e
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-15 21:48:06 +0300

    Create O6.cpp

---
+++ Math/O6.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    if (20 > 18) {
+        cout << "20 is greater than 18";
+    }
+    return 0;
+}
+/*In the example below, we test two values to find out if 20 is greater than 18. If the condition is true, print some text:*/
