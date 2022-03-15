6b5eaab48a24f76560d3e0771de05e2a9fe9f183
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-15 10:55:58 +0300

    Create O8.cpp

---
+++ Strings/O8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string myString = "Hello";
+	myString[0] = 'J';
+	cout << myString;
+	return 0;
+}
+//To change the value of a specific character in a string, refer to the index number, and use single quotes:
