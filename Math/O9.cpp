7bbb7825bc72338dad89c48ba5d6d83f528f4ca5
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-15 21:53:00 +0300

    Create O9.cpp

---
+++ Math/O9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  int time = 20;
+  string result = (time < 18) ? "Good day." : "Good evening.";
+  cout << result;
+  return 0;
+}
+/*Short Hand If...Else (Ternary Operator)
+There is also a short-hand if else, which is known as the ternary operator because it consists of three operands. 
+It can be used to replace multiple lines of code with a single line. It is often used to replace simple if else statements:
+Syntax
+variable = (condition) ? expressionTrue : expressionFalse;*/
