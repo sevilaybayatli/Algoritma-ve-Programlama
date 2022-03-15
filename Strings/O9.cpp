aac741420875c6684f051bf02298fa17a9ec0b5d
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-15 11:15:05 +0300

    Create O9.cpp

---
+++ Strings/O9.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string firstName;
+	cout << "Type your first name: ";
+	cin >> firstName; // get user input from the keyboard
+	cout << "Your name is: " << firstName;
+}
+// Type your first name: John
+// Your name is: John
