a048590a7426889aebf73188a1f9a762c37863f5
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-15 10:49:42 +0300

    Create O7.cpp

---
+++ Strings/O7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string myString = "Hello";
+	cout << myString[0];
+	return 0;
+}
+//You can access the characters in a string by referring to its index number inside square brackets[].
+//The example above prints the first character in myString:
