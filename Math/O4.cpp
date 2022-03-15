a9b7ec61d6c17e92d57a8b6cf18bfef36fae6ce8
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-15 21:41:56 +0300

    Create O4.cpp

---
+++ Math/O4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x = 10;
+	int y = 9;
+	cout << (x > y);
+	return 0;
+}
+/*Boolean Expression
+A Boolean expression is a C++ expression that returns a boolean value: 1 (true) or 0 (false).
+
+You can use a comparison operator, such as the greater than (>) operator to find out if an expression (or a variable) is true:*/
