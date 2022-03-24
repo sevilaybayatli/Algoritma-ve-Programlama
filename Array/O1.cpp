c846a0ba141c09560945753153f255cd451306e7
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-24 22:12:21 +0300

    Create O1.cpp

---
+++ Array/O1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+  cout << cars[0];
+  return 0;
+}
+
+/*You access an array element by referring to the index number inside square brackets [].
+
+This statement accesses the value of the first element in cars:*/
