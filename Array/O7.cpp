e570959649b0c74f4dff725914d61cf5b7863e5d
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-24 22:47:15 +0300

    Create O7.cpp

---
+++ Array/O7.cpp
@@ -0,0 +1,9 @@
+/*To get the size of an array, you can use the sizeof() operator:*/
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNumbers[5] = {10, 20, 30, 40, 50};
+  cout << sizeof(myNumbers);
+  return 0;
+}
