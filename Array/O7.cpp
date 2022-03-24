e54e99e38573961833c73392eb8371eb839ff463
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-24 22:48:45 +0300

    Update O7.cpp

--- Array/O7.cpp
+++ Array/O7.cpp
@@ -7,3 +7,9 @@ int main() {
   cout << sizeof(myNumbers);
   return 0;
 }
+
+/*Why did the result show 20 instead of 5, when the array contains 5 elements?
+
+It is because the sizeof() operator returns the size of a type in bytes.
+
+You learned from the Data Types chapter that an int type is usually 4 bytes, so from the example above, 4 x 5 (4 bytes x 5 elements) = 20 bytes.*/
