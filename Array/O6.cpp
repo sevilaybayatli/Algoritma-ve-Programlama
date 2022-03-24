caa99352ae56b15a7bca337eba7597586e7a59fd
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-24 22:46:06 +0300

    Create O6.cpp

---
+++ Array/O6.cpp
@@ -0,0 +1,18 @@
+/*Omit Elements on Declaration
+It is also possible to declare an array without specifying the elements on declaration, and add them later:*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string cars[5];
+  cars[0] = "Volvo";
+  cars[1] = "BMW";
+  cars[2] = "Ford";
+  cars[3] = "Mazda";
+  cars[4] = "Tesla";
+  for(int i = 0; i < 5; i++) {
+    cout << cars[i] << "\n";
+  }
+  return 0;
+}
