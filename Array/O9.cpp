77a92180ceadbdcc939e0c2987e3e71eb5a98f35
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-29 11:47:59 +0300

    Create O9.cpp

---
+++ Array/O9.cpp
@@ -0,0 +1,14 @@
+/*This statement accesses the value of the element in the first row (0) and third column (2) of the letters array.*/
+#include<iostream>
+using namespace std;
+
+int main() {
+    string letters[2][4] = {
+  { "A", "B", "C", "D" },
+  { "E", "F", "G", "H" }
+    };
+
+    cout << letters[0][2];
+    return 0;
+
+}
