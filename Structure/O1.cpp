d661f4c2cb0268925801d11b8e6d84e2b0b30b23
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-03-29 13:05:09 +0300

    Create O1.cpp

---
+++ Structure/O1.cpp
@@ -0,0 +1,18 @@
+/*Assign data to members of a structure and print it:*/
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    struct {
+        int myNum;
+        string myString;
+    } myStructure;
+
+    myStructure.myNum = 1;
+    myStructure.myString = "Hello World!";
+
+    cout << myStructure.myNum << "\n";
+    cout << myStructure.myString << "\n";
+    return 0;
+}
