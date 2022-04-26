d473dd390423bf0b0d61bcf0c97e8d87a66fff9c
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-04-26 19:14:21 +0300

    Create O1.cpp

---
+++ Functions/O1.cpp
@@ -0,0 +1,13 @@
+//Inside main, call myFunction():
+
+#include <iostream>
+using namespace std;
+
+void myFunction() {
+  cout << "I just got executed!";
+}
+
+int main() {
+  myFunction();
+  return 0;
+}
