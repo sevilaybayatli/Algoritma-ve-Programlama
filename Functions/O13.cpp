3cf6b7c5910c32275fef9b2466ad546c31ea5b85
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-05-10 13:56:08 +0300

    Create O13.cpp

---
+++ Functions/O13.cpp
@@ -0,0 +1,19 @@
+//Consider the following example, which have two functions that add numbers of different type:
+#include <iostream>
+using namespace std;
+
+int plusFuncInt(int x, int y) {
+  return x + y;
+}
+
+double plusFuncDouble(double x, double y) {
+  return x + y;
+}
+
+int main() {
+  int myNum1 = plusFuncInt(8, 5);
+  double myNum2 = plusFuncDouble(4.3, 6.26);
+  cout << "Int: " << myNum1 << "\n";
+  cout << "Double: " << myNum2;
+  return 0;
+}
