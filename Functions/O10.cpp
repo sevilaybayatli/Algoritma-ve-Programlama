7ea9ac7fb703f4d7b0206a6d41c7eb7432e84077
sevilay bayatlı <sevilaybayatli@gmail.com>
2022-05-10 13:39:07 +0300

    Create O10.cpp

---
+++ Functions/O10.cpp
@@ -0,0 +1,13 @@
+//You can also store the result in a variable:
+#include <iostream>
+using namespace std;
+
+int myFunction(int x, int y) {
+  return x + y;
+}
+
+int main() {
+  int z = myFunction(5, 3);
+  cout << z;
+  return 0;
+}
